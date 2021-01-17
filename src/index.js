e4387d770be3375f3e53ea613d080be6d1d25f6e
Alexandra Shestakova <shestakova18@bk.ru>
2021-01-17 17:52:51 +0300

    feat: added default global configuration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
 axios.interceptors.request.use(
   (request) => {
     console.log(request);
